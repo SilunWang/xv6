ef9f216a220f99b12d781020528dcd5ead0d8e55
SilunWang <wangsl11@mails.tsinghua.edu.cn>
2013-12-01 13:09:34 +0800

    sound.c addSound() palySound()

--- sound.c
+++ sound.c
@@ -1,9 +1,38 @@
 #include "types.h"
 #include "defs.h"
 #include "x86.h"
+#include "spinlock.h"
+#include "sound.h"
 
 #define PCI_CONFIG_COMMAND 0xcf8
 #define PCI_CONFIG_DATA 0xcfc
+
+//Native Audio Mixer Base Address
+ushort AUDIO_IO_SPACE_NAMBA;
+#define GENERAL_PURPO 0x20 //General Purpose
+
+//Native Audio Bus Mastering Base Address
+ushort AUDIO_IO_SPACE_NABMBA;
+#define PO_BDBAR 0x10//Buffer基地址 PCM Out Buffer Descriptor list Base Address Register 
+#define PO_CIV 0x14//没用上 PCM Out Current Index Value
+#define PO_LVI 0x15//PCM Out Last Valid Index 
+#define PO_SR 0x16//PCM Out Status Register
+#define PO_CR 0x1B //PCM Out Control Register
+#define MC_BDBAR 0x20//Mic. In Buffer Descriptor list Base Address Register 
+#define PM_CIV 0x24//Mic. In Current Index Value 
+#define MC_LVI 0x25//Mic. In Last Valid Index
+#define MC_SR 0x26//Mic. In Status Register
+#define MC_CR 0x2B//Mic. In Control Register
+
+static struct spinlock soundLock;
+static struct soundNode *soundQueue;
+
+struct descriptor{
+  uint buf;
+  uint cmd_len;
+};
+
+static struct descriptor desTable[DMA_BUF_NUM];
 
 uint read_pci_config(uchar bus, uchar slot, uchar func, uchar offset)
 {
@@ -43,4 +72,121 @@ void soundinit(void)
 					}
 				}
 			}
+}
+
+void addSound(struct soundNode *node)
+{
+  struct soundNode *ptr;
+
+  acquire(&soundLock);
+
+  node->next = NULL;
+  ptr = soundQueue;
+
+  while(ptr)
+  {
+  	ptr = ptr->next;
+  }
+  ptr = node;
+
+  if(soundQueue == node)
+  {
+    playSound();
+  }
+
+  release(&soundLock);
+}
+
+void playSound()
+{
+	int i;
+
+	//遍历声卡DMA的描述符列表，初始化每一个描述符buf指向缓冲队列中第一个音乐的数据块
+	//每个数据块大小: DMA_BUF_SIZE
+	for (i = 0; i < DMA_BUF_NUM; i++)
+	{
+    	desTable[i].buf = (uint)(soundQueue->data) + i * DMA_BUF_SIZE;
+    	desTable[i].cmd_len = 0x80000000 + DMA_SMP_NUM;
+	}
+
+	uint base = (uint)desTable;
+
+	//开始播放
+	if ((soundQueue->flag & PCM_OUT) == PCM_OUT)
+	{
+    	//init base register
+    	outsl(AUDIO_IO_SPACE_NABMBA + PO_BDBA, &base, 1);
+    	//init last valid index
+    	outb(AUDIO_IO_SPACE_NABMBA + MC_LVI, 0x1F);
+    	//init control register
+    	outb(AUDIO_IO_SPACE_NABMBA + MC_CR, 0x05);
+	}
+
+	//开始录音
+	else if ((soundQueue->flag & PCM_IN) == PCM_IN)
+	{
+    	//init register
+    	outsl(AUDIO_IO_SPACE_NABMBA + PO_BDBA, &base, 1);
+    	outb(AUDIO_IO_SPACE_NABMBA + MC_LVI, 0x1F);
+    	outb(AUDIO_IO_SPACE_NABMBA + MC_CR, 0x05);
+	}
+
+}
+
+void soundInterrupt(void)
+{
+  int i;
+
+  acquire(&soundLock);
+
+  struct soundNode *node = soundQueue;
+  soundQueue = node->next;
+
+  //flag
+  int flag = node->flag;
+
+  node->flag |= PROCESSED;
+
+  //0 sound file left
+  if (soundQueue == 0)
+  {
+    if ((flag & PCM_OUT) == PCM_OUT)
+    {
+      ushort sr = inw(AUDIO_IO_SPACE_NABMBA + PO_SR);
+      outw(AUDIO_IO_SPACE_NABMBA + PO_SR, sr);
+    }
+    else if ((flag & AB_PCM_IN) == PCM_IN)
+    {
+      ushort sr = inw(AUDIO_IO_SPACE_NABMBA + MC_SR);
+      outw(AUDIO_IO_SPACE_NABMBA + MC_SR, sr);
+    }
+    cprintf("Play Done\n");
+    release(&soundLock);
+    return;
+  }
+
+  //descriptor table buffer
+  for (i = 0; i < DMA_BUF_NUM; i++)
+  {
+    desTable[i].buf = (uint)(soundQueue->data) + i * DMA_BUF_SIZE;
+    desTable[i].cmd_len = 0x80000000 + DMA_SMP_NUM;
+  }
+
+  //play music
+  if ((flag & PCM_OUT) == PCM_OUT)
+  {
+    ushort sr = inw(AUDIO_IO_SPACE_NABMBA + PO_SR);
+    outw(AUDIO_IO_SPACE_NABMBA + PO_SR, sr);
+    outb(AUDIO_IO_SPACE_NABMBA + PO_CR, 0x05);
+  }
+
+  //record
+  else if ((flag & PCM_IN) == PCM_IN)
+  {
+    ushort sr = inw(AUDIO_IO_SPACE_NABMBA + MC_SR);
+    outw(AUDIO_IO_SPACE_NABMBA + MC_SR, sr);
+    outb(AUDIO_IO_SPACE_NABMBA + MC_CR, 0x05);
+  }
+  
+  release(&soundLock);
 }

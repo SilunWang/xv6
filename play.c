7736cc1d3feb3975cd2af9849a7ea3c61e393ecf
xu-wang11 <wangxu.93@icloud.com>
2013-12-01 19:32:58 +0800

    play.c

--- play.c
+++ play.c
@@ -1,27 +1,60 @@
 #include "types.h"
-#include "defs.h"
-#include "param.h"
 #include "stat.h"
-#include "mmu.h"
-#include "proc.h"
-#include "spinlock.h"
-#include "buf.h"
-#include "fs.h"
-#include "file.h"
-void play(char* file)
-{
+#include "user.h"
+#include "audio.h"
+#include "fcntl.h"
 
-}
-int main(int argc, char** argv)
+int
+main(int argc, char *argv[])
 {
-	if(argc != 2)
-	{
-		printf("error format parameter");
-	}
-	else
-	{
-		play(argv[1]);
-	}
-	exit();
+  int i;
+  int fd;
 
+  struct wav info;
+
+  fd = open(argv[1], O_RDWR);
+  if (fd < 0)
+  {
+    printf(0, "open wav file fail\n");
+    exit();
+  }
+
+  read(fd, &info, sizeof(struct wav));
+
+  if ((info.riff_id != 0x46464952)||(info.wave_id != 0x45564157)) {
+    printf(0, "invalid file format\n");
+    close(fd);
+    exit();
+  }
+
+  if ((info.info.id != 0x20746d66)||
+      (info.info.channel != 0x0002)||
+      (info.info.bytes_per_sample != 0x0004)||
+      (info.info.bits_per_sample != 0x0010)) {
+    printf(0, "data encoded in an unaccepted way\n");
+    close(fd);
+    exit();
+  }
+
+  setaudiosmprate(info.info.sample_rate);
+
+  uint rd = 0;
+  char buf[4096];
+  
+  while (rd < info.dlen)
+  {
+    read(fd, buf, (info.dlen - rd < 4096 ? info.dlen -rd : 4096));
+    writeaudio(buf, (info.dlen - rd < 4096 ? info.dlen -rd : 4096));
+    rd += (info.dlen - rd < 4096 ? info.dlen -rd : 4096);
+  }
+
+  memset(buf, 0, 4096);
+  for (i = 0; i < DMA_BUF_NUM*DMA_BUF_SIZE/4096+1; i++)
+  {
+    writeaudio(buf, 4096);
+  }
+
+  close(fd);
+
+  exit();
 }
